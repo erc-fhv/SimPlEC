1661531412062611dcf5f16daeaccdb56f6f0dc1
Valentin Seiler <valentin.seiler@fhv.at>
2024-11-20 09:42:23 +0100

    added readme to pip in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
-# Read the contents of your README file
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="simplec",
@@ -10,8 +9,8 @@ setup(
     author="Valentin Seiler",
     author_email="valentin.seiler@fhv.at",
     description="Simulation Playground for Energy Communities (time discrete and evend discrete cosimulaton)",
-    long_description='',
-    # long_description_content_type="text/markdown",  # Use README.md as long_description
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Use README.md as long_description
     # url="https://github.com/your_username/your_repo_name",  # Project URL
     packages=['simplec_examples'],  # find_packages(),  # Automatically find packages in the directory
     py_modules=['simplec'],
